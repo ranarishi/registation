70541b1e6a6ff3b1eab1a87cc09d8b18b785a977
ranarishi <167693726+ranarishi@users.noreply.github.com>
2024-04-26 21:38:16 +0530

    added index.php

---
+++ index.php
@@ -0,0 +1,93 @@
+<?php
+include("header.php");
+
+?>
+
+<section>
+<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
+  </div>
+  <div class="carousel-inner slide">
+    <div class="carousel-item active">
+      <img src="image/slide-1.jpg" class="d-block " alt="..." width="1300" height="500">
+    </div>
+    <div class="carousel-item">
+      <img src="image/slide-4.jpg" class="d-block " alt="..." width="1300" height="500">
+    </div>
+    <div class="carousel-item">
+      <img src="image/slide-3.jpg" class="d-block " alt="..." width="1300" height="500">
+    </div>
+    
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+</section>
+
+<div class="container py-5">
+  <div class="row">
+    <div class="col-sm-12 text-center">
+      <h2>About Us</h2>
+      <hr>
+</div>
+      <div class="col-sm-8">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur consequuntur, delectus voluptatibus maiores commodi facere, vitae a earum temporibus optio expedita reiciendis quaerat nostrum dolore. Dolorum id nesciunt repudiandae odit hic eos .</p>
+
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatibus doloremque libero asperiores atque iusto? Illo dolorem autem reprehenderit culpa quos alias corporis ullam quo .</p>
+      </div>
+      <div class="col-sm-4">
+        <figure>
+        <img class="img-fluid" src="image/cart.jpg" alt="">
+        </figure>
+      </div>
+
+      <div class="col-sm-4">
+        <figure>
+        <img class="img-fluid" src="image/cart.jpg" alt="">
+        </figure>
+      </div>
+
+      <div class="col-sm-8">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur consequuntur, delectus voluptatibus maiores commodi facere, vitae a earum temporibus optio expedita reiciendis quaerat nostrum dolore. Dolorum id nesciunt repudiandae odit hic eos .</p>
+
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatibus doloremque libero asperiores atque iusto? Illo dolorem autem reprehenderit culpa quos alias corporis ullam quo .</p>
+      </div>
+
+      <div class="col-sm-8">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur consequuntur, delectus voluptatibus maiores commodi facere, vitae a earum temporibus optio expedita reiciendis quaerat nostrum dolore. Dolorum id nesciunt repudiandae odit hic eos .</p>
+
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatibus doloremque libero asperiores atque iusto? Illo dolorem autem reprehenderit culpa quos alias corporis ullam quo .</p>
+      </div>
+      <div class="col-sm-4">
+        <figure>
+        <img class="img-fluid" src="image/cart.jpg" alt="">
+        </figure>
+      </div>
+
+      <div class="col-sm-4">
+        <figure>
+        <img class="img-fluid" src="image/cart.jpg" alt="">
+        </figure>
+      </div>
+      <div class="col-sm-8">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur consequuntur, delectus voluptatibus maiores commodi facere, vitae a earum temporibus optio expedita reiciendis quaerat nostrum dolore. Dolorum id nesciunt repudiandae odit hic eos .</p>
+
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatibus doloremque libero asperiores atque iusto? Illo dolorem autem reprehenderit culpa quos alias corporis ullam quo .</p>
+      </div>
+
+    </div>
+  </div>
+</div>
+
+<?php
+include('footer.php');
